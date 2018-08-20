aff8b20b39e47faaea7e3473a61e0751846e63d6
ashu95 <ghildiyal.a@husky.neu.edu>
2018-08-20 00:23:39 -0400

    server.js added

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/webdev2-angular-client1'));
+app.use(express.static(__dirname + '/dist/Rev-Socia/index.html'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/webdev2-angular-client1/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/Rev-Socia/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
